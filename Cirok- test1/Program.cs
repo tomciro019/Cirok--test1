6f7455952e96fefbabff73185ec5ddf88cffe67a
Tomáš Cirók <u19.402@pslib.cloud>
2020-09-29 08:57:11 +0200

    konečná upraava

--- Cirok- test1/Program.cs
+++ Cirok- test1/Program.cs
@@ -2,27 +2,24 @@
 
 namespace Cirok__test1
 {
-    class class
-    { 
-        
+    class program
+    {
+
         static void Main(string[] args)
         {
-        n = Convert.ToInt32(Console.ReadLine());   
-          Console.WriteLine(PowerOfTwo(Convert.ToInt32(Console.ReadLine())));
-
-
-
-            
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(PowerOfTwo(Convert.ToInt32(Console.ReadLine())));
         }
 
         static int PowerOfTwo(int n)
         {
             int result = n * n;
             return result;
-        
-
 
         }
-      }
+
+    }
+}
+
     
       
